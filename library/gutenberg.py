379db181ede784146be1a5c4818d05d3c0f80d01
emechtel <108358333+emechtel@users.noreply.github.com>
2022-07-04 04:01:55 -0400

    Rework gutenberg.py
    Made the program functional, which functionally extracts the slices from the librarian.txt file and then assembles the output data in the output directory.

--- library/gutenberg.py
+++ library/gutenberg.py
@@ -9,36 +9,49 @@ NOT like coordinates in xyz space
 '''
 #NOTE this script is not commented, rather the commetned text represents old commands kept for posterity
 
-num = 20
+def prose():
+    import os
+    os.chdir(os.path.dirname(__file__))
+    os.chdir(os.path.dirname(os.getcwd()))
+    with open('librarian.txt') as callnumber:
+        stories = callnumber.read().replace('\n',' ')
+    callnumber.close()
+    lines = stories[9:11]
+    meter = int(lines)
+    return meter
 
-import os
-import numpy as np
-#from scipy import io 
-
-row = str(os.path.dirname(__file__) + '\\nonfiction')
-try:
-    os.chdir(row)
-    for f in os.listdir(row):
-        os.remove(os.path.join(row, f))
-    print("Repository cleared")
-except:
-    lib = os.path.dirname(__file__)
-    os.chdir(lib)
-    os.makedirs(row)
-    os.chdir(row)
-    print("Repository cleared")
-finally:
-    dewey = np.zeros((num,num))
-    for z in range(num):
-        for x in range (num):
-            for y in range (num):
-                dewey[x][y] = np.float32(((x+0.5)**2+(y+0.5)**2+(z+0.5)**2)**(0.5))
-        print(str(z/num*100)+'%')
-        # io.savemat('distancesz%s.mat' % str(z+1),{"unfiltereddistances": q})
-        np.savetxt('shelf_no%s.csv' % str(z+1), dewey, delimiter=',',)
-        del dewey
+def collate():
+    import os
+    import numpy as np
+    #from scipy import io 
+    num = prose()
+    row = str(os.path.dirname(__file__) + '\\nonfiction')
+    try:
+        os.chdir(row)
+        for f in os.listdir(row):
+            os.remove(os.path.join(row, f))
+        print("Repository cleared")
+    except:
+        lib = os.path.dirname(__file__)
+        os.chdir(lib)
+        os.makedirs(row)
+        os.chdir(row)
+        print("Repository cleared")
+    finally:
         dewey = np.zeros((num,num))
-    print('100%')
-    #q.astype(np.uint32).tofile('test.bin') #for more compact data sets, set uint[integer] to best value
-    #io.savemat('distances.mat',{"unfiltereddistances": q})
-    print('Done')
+        for z in range(num):
+            for x in range (num):
+                for y in range (num):
+                    dewey[x][y] = np.float32(((x+0.5)**2+(y+0.5)**2+(z+0.5)**2)**(0.5))
+            print(str(z/num*100)+'%')
+            # io.savemat('distancesz%s.mat' % str(z+1),{"unfiltereddistances": q})
+            np.savetxt('shelf_no%s.csv' % str(z+1), dewey, delimiter=',',)
+            del dewey
+            dewey = np.zeros((num,num))
+        print('100%')
+        #q.astype(np.uint32).tofile('test.bin') #for more compact data sets, set uint[integer] to best value
+        #io.savemat('distances.mat',{"unfiltereddistances": q})
+        print('Done')
+
+
+collate()
