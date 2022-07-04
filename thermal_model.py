acf5c8d239ac3a7022a57520badc97302ec2c035
emechtel <108358333+emechtel@users.noreply.github.com>
2022-07-04 18:55:49 -0400

    Update thermal_model.py
    Attempt at os and sys library patch

--- thermal_model.py
+++ thermal_model.py
@@ -5,8 +5,12 @@ sys.dont_write_bytecode=True # stops the __pycache__ folder from spawning
 os.chdir(os.path.dirname(__file__)) # make current directory the working directory
 
 def author():   # since this is the field deployable version, this will assemble the distance matrices remotely
-    lib = str(os.path.dirname(__file__) + 'library')
-    sys.path.insert(1, lib)
+    # lib = str(os.path.dirname(__file__) + 'library')
+    # sys.path.insert(1, lib)
+    os.chdir(os.path.dirname(__file__) + "\library")
     from gutenberg import collate
+    os.chdir(os.path.dirname(__file__))
+
+collate()
 
 
