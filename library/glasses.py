f1fcffe5b53e28cab56c4dd28b4ab4e49cdfee91
emechtel <108358333+emechtel@users.noreply.github.com>
2022-07-04 01:55:34 -0400

    Add files via upload
    File reupload from hard copy.

---
+++ library/glasses.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+from PIL import Image as im
+
+row = str(os.path.dirname(__file__) + '\\nonfiction')
+num = len([entry for entry in os.listdir(row) if os.path.isfile(os.path.join(row, entry))])
+
+val = 13#input("There are currently %s slices available for viewing.\nEnter slice value starting at 1: " % str(num))
+
+shelf = np.loadtxt(str(row + "\\shelf_no%d.csv" % int(val)), delimiter=',')
+
+max = np.max(shelf)
+min = np.min(shelf)
+range = max-min
+step = 255/range
+
+image_data = np.uint8(step*(shelf-min))
+
+os.chdir(os.path.dirname(__file__))
+
+book = im.fromarray(image_data)
+book.save(str(os.path.dirname(__file__) + "\\book.jpeg"))
+booklet = im.open(str(os.path.dirname(__file__) + "\\book.jpeg"))
+booklet.show('Value Gradient')
+os.remove(str(os.path.dirname(__file__) + "\\book.jpeg"))
