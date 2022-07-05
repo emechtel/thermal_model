a2f434c14f6a4fc9342608b9a9b117182db80100
emechtel <emechtel@umd.edu>
2022-07-05 16:51:31 -0400

    Update thermal_model.py
    Compatible with appending path limits, no longer needs directory change.

--- thermal_model.py
+++ thermal_model.py
@@ -4,15 +4,11 @@ import os
 sys.dont_write_bytecode=True # stops the __pycache__ folder from spawning
 os.chdir(os.path.dirname(__file__)) # make current directory the working directory
 
-def collate():   # since this is the field deployable version, this will assemble the distance matrices remotely
-    # lib = str(os.path.dirname(__file__) + 'library')
-    # sys.path.insert(1, lib)
-    os.chdir(os.path.dirname(__file__) + "\library")
+def author():   # since this is the field deployable version, this will assemble the distance matrices remotely
+    lib = str(os.path.dirname(__file__) + '\library')
+    sys.path.append(lib)
     from gutenberg import collate
-    os.chdir(os.path.dirname(__file__))
 
-collate()
-
-#this is a test
+author()
 
 
