f616302acd091ca1fa5468f7e81ce6a0682a470d
emechtel <emechtel@umd.edu>
2022-07-05 18:18:12 -0400

    Update thermal_model.py
    Made author function directory compatible.

--- thermal_model.py
+++ thermal_model.py
@@ -5,9 +5,11 @@ sys.dont_write_bytecode=True # stops the __pycache__ folder from spawning
 os.chdir(os.path.dirname(__file__)) # make current directory the working directory
 
 def author():   # since this is the field deployable version, this will assemble the distance matrices remotely
+    os.chdir(os.path.dirname(__file__) + '\library')
     lib = str(os.path.dirname(__file__) + '\library')
     sys.path.append(lib)
     from gutenberg import collate
+    os.chdir(os.path.dirname(__file__))
 
 author()
 
