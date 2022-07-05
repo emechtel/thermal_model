9bff4e355dfcee464484940308b8d2ef7db5de3f
emechtel <108358333+emechtel@users.noreply.github.com>
2022-07-05 16:16:27 -0400

    Update thermal_model.py
    updated function name to correspond to locally saved code

--- thermal_model.py
+++ thermal_model.py
@@ -4,7 +4,7 @@ import os
 sys.dont_write_bytecode=True # stops the __pycache__ folder from spawning
 os.chdir(os.path.dirname(__file__)) # make current directory the working directory
 
-def author():   # since this is the field deployable version, this will assemble the distance matrices remotely
+def collate():   # since this is the field deployable version, this will assemble the distance matrices remotely
     # lib = str(os.path.dirname(__file__) + 'library')
     # sys.path.insert(1, lib)
     os.chdir(os.path.dirname(__file__) + "\library")
