40c75d2a62aa8e50c954e49d1eb3c35b7f42f717
emechtel <emechtel@umd.edu>
2022-07-05 18:23:07 -0400

    Update thermal_model.py
    Updated python version compatibility and commented.

--- thermal_model.py
+++ thermal_model.py
@@ -5,11 +5,13 @@ sys.dont_write_bytecode=True # stops the __pycache__ folder from spawning
 os.chdir(os.path.dirname(__file__)) # make current directory the working directory
 
 def author():   # since this is the field deployable version, this will assemble the distance matrices remotely
-    os.chdir(os.path.dirname(__file__) + '\library')
-    lib = str(os.path.dirname(__file__) + '\library')
-    sys.path.append(lib)
-    from gutenberg import collate
-    os.chdir(os.path.dirname(__file__))
+    lib = str(os.path.dirname(__file__) + '\library') # define library folder path
+    os.chdir(lib) # move to directory for compatibility with older python versions
+    sys.path.append(lib) # add path to PATH
+    from gutenberg import collate # import and deploy distance repository and file creation function
+    sys.path.remove(lib) # remove path from PATH to prevent unwanted searching for future scripts
+    os.chdir(os.path.dirname(__file__)) # return to current file directory to run rest of script
+    
 
 author()
 
