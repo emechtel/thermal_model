3f1e5fc7aef0b8952f8e570dc1dfa940b0d2393d
emechtel <emechtel@umd.edu>
2022-07-05 19:37:43 -0400

    Update thermal_model.py
    Changed function names to reflect deployability function.

--- thermal_model.py
+++ thermal_model.py
@@ -4,7 +4,7 @@ import os
 sys.dont_write_bytecode=True # stops the __pycache__ folder from spawning
 os.chdir(os.path.dirname(__file__)) # make current directory the working directory
 
-def author():   # since this is the field deployable version, this will assemble the distance matrices remotely
+def deployable():   # since this is the field deployable version, this will assemble the distance matrices remotely
     lib = str(os.path.dirname(__file__) + '\library') # define library folder path
     os.chdir(lib) # move to directory for compatibility with older python versions
     sys.path.append(lib) # add path to PATH
@@ -13,6 +13,6 @@ def author():   # since this is the field deployable version, this will assemble
     os.chdir(os.path.dirname(__file__)) # return to current file directory to run rest of script
     
 
-author()
+
 
 
